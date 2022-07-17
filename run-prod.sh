89a182b070ae6a9909bc0fb369d856820c1a7bda
dgop92 <inevaup@gmai.com>
2022-07-17 18:32:03 -0500

    config: add prod compose file

--- run-prod.sh
+++ run-prod.sh
@@ -3,4 +3,4 @@
 python manage.py wait_for_db &&
 python manage.py makemigrations &&
 python manage.py migrate &&
-python manage.py runserver 0.0.0.0:8080
+gunicorn --bind 0.0.0.0:8080 find_your_gap_api.wsgi
